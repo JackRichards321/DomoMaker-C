f06953ddcfc061064d5cd5743ee6262bbf9b16dd
JackRichards321 <sjackrichards88@gmail.com>
2021-04-12 15:29:56 -0400

    test commit

--- server/app.js
+++ server/app.js
@@ -9,42 +9,42 @@ const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const url = require('url');
-const redis = require ('redis');
+const redis = require('redis');
 const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+// const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 const dbURL = 'mongodb+srv://jackRichards:mongoDuck@cluster0.lbmem.mongodb.net/DomoMaker';
 
 // setup mongoose options to use newer functionality
 const mongooseOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
-    if(err) {
-        console.log('Could not connect to database');
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 let redisURL = {
-    hostname: 'redis-17272.c262.us-east-1-3.ec2.cloud.redislabs.com',
-    port: 17272,
+  hostname: 'redis-17272.c262.us-east-1-3.ec2.cloud.redislabs.com',
+  port: 17272,
 };
 
 let redisPASS = 'GauDZto2eqw3FeBWPKmgzE6ah4f4AFvz';
 if (process.env.REDISCLOUD_URL) {
-    redisURL = url.parse(process.env.REDISCLOUD_URL);
-    [, redisPass] = redisURL.auth.split(':');
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPASS] = redisURL.auth.split(':');
 }
-let redisClient = redis.createClient({
-    host: redisURL.hostname,
-    port: redisURL.port,
-    password: redisPASS,
+const redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
 });
 
 // pull in our routes
@@ -56,19 +56,19 @@ app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
 app.use(session({
-    key: 'sessionid',
-    store: new RedisStore({
-        client: redisClient,
-    }),
-    secret: 'Domo Arigato',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-    },
+  key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -80,18 +80,17 @@ app.use(cookieParser());
 // should come BEFORE the router
 app.use(csrf());
 app.use((err, req, res, next) => {
-    if (err.code !== 'EBADCSRFTOKEN') return next(err);
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
 
-    console.log('Missing CSRF token');
-    return false;
+  console.log('Missing CSRF token');
+  return false;
 });
 
 router(app);
 
 app.listen(port, (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log(`listening on port ${port}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`listening on port ${port}`);
 });
-
